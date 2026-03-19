a961033a37f49302ccdd9b16723b80b55e36c4a8
Jeevan Gowda <jeevannr15@gmail.com>
2026-03-19 08:17:55 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation //
+var typed = new Typed(".typing", {
+    strings:["","Data Analyst", "Frontend Developer", "SQL Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
