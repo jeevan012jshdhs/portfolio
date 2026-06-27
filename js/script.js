09ccaa7b8350222cbcf27d6183b2fcfec3e61c7d
Jeevan N R <163236029+jeevan012jshdhs@users.noreply.github.com>
2026-06-27 18:20:03 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Typing Animation //
-var typed = new Typed(".typing", {
-    strings:["","Data Analyst", "Frontend Developer", "SQL Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
